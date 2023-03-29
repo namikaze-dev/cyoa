d4ce512074f9552e053f1ac236d3a422b539258f
namikaze-dev <usmansadik96@gmail.com>
2023-03-29 12:50:28 +0100

    Impl Handler for processing requests

---
+++ cmd/web/handler.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+
+	"github.com/namikaze-dev/cyoa/views"
+)
+
+type Handler struct {
+	Store map[string]Story
+}
+
+const defaultArcName = "intro"
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	arcName := r.URL.Path
+	if arcName == "/" {
+		arcName = defaultArcName
+	} else {
+		arcName = strings.TrimLeft(arcName, "/")
+	}
+
+	story, ok := h.Store[arcName]
+	if !ok {
+		http.Error(w, fmt.Sprintf("story with name: %q does not exist", arcName), http.StatusBadRequest)
+		return
+	}
+
+	h.render(w, http.StatusOK, "index", &story)
+}
+
+func (h *Handler) render(w http.ResponseWriter, status int, page string, data *Story) {
+	ts, err := template.ParseFS(views.Files, fmt.Sprintf("html/%v.html", page))
+	if err != nil {
+		fmt.Println("cyoa:", fmt.Errorf("the template %s does not exist", page))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	buf := &bytes.Buffer{}
+	err = ts.Execute(buf, data)
+	if err != nil {
+		fmt.Println("cyoa:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(status)
+	buf.WriteTo(w)
+}
---
+++ cmd/web/handler_test.go
@@ -0,0 +1,101 @@
+package main_test
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	web "github.com/namikaze-dev/cyoa/cmd/web"
+)
+
+func TestHandler(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rr := httptest.NewRecorder()
+
+		handler := web.Handler{
+			Store: testStoryStore,
+		}
+		handler.ServeHTTP(rr, req)
+
+		resp := rr.Result()
+		defer resp.Body.Close()
+
+		assertEqual(t, resp.StatusCode, http.StatusOK)
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("unexpected error from io.ReadAll: %v", err)
+		}
+		body = bytes.TrimSpace(body)
+
+		want := "Title 1"
+		if !bytes.Contains(body, []byte(want)) {
+			t.Errorf("want body containing %q, got %q", want, string(body))
+		}
+		
+		want = "<html"
+		if !bytes.Contains(body, []byte(want)) {
+			t.Errorf("want body containing %q, got %q", want, string(body))
+		}
+	})
+
+	t.Run("bad request", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/bad-arc-name", nil)
+		rr := httptest.NewRecorder()
+
+		handler := web.Handler{
+			Store: testStoryStore,
+		}
+		handler.ServeHTTP(rr, req)
+
+		resp := rr.Result()
+		defer resp.Body.Close()
+
+		assertEqual(t, resp.StatusCode, http.StatusBadRequest)
+	})
+}
+
+func assertEqual(t *testing.T, got, want any) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %#v want %#v", got, want)
+	}
+}
+
+var testStoryStore = map[string]web.Story{
+	"intro": {
+		Title: "Title 1",
+		Story: []string{
+			"story line 1",
+			"story line 2",
+			"story line 3",
+		},
+		Options: []web.StoryOption{
+			{
+				Text: "text 1",
+				Arc:  "valley",
+			},
+		},
+	},
+	"valley": {
+		Title: "Valley of Code",
+		Story: []string{
+			"story line 1",
+			"story line 2",
+		},
+		Options: []web.StoryOption{
+			{
+				Text: "start",
+				Arc:  "intro",
+			},
+			{
+				Text: "end",
+				Arc:  "end-arc",
+			},
+		},
+	},
+}
